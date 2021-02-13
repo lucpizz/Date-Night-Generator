ded72001fb5513d6595aa9fbd5014651dd0704a0
Lucpizz <Lucpizz@gmail.com>
2021-02-13 11:52:20 -0500

    adding receipe API code

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,9 @@
+/*
+ 
+ This API get is a song finder
+
+*/
+
 const settings = {
 	"async": true,
 	"crossDomain": true,
@@ -11,3 +17,37 @@ const settings = {
 $.ajax(settings).done(function (response) {
 	console.log(response);
 });
+
+
+/*
+
+This API get is for food receipes
+
+*/
+
+function spoonApiCall() {
+  let api_Key = "";
+  let endpoint = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${api_Key}&includeIngredients=${ingredients}`;
+
+  $.ajax({
+    url: endpoint,
+    method: "GET",
+    error: function (XMLHttpRequest, textStatus, errorThrown) {
+      alert(
+        "Sorry, but this search generated an error.  Please try again! " +
+          "\n Details: Status =" +
+          XMLHttpRequest.status +
+          ", Status Text: " +
+          XMLHttpRequest.statusText
+      );
+      housekeeping();
+      return;
+    },
+  }).then(function (response) {
+    JSON.stringify.response;
+    console.log("JSON Spoontacular Payload: " + response);
+    spoonLength = response.results.length;
+    displayResults(response, spoonLength);
+  });
+  console.log("Length of JSON-returned list " + spoonLength);
+}
