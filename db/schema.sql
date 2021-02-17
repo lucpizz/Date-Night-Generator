a22cb91ee651b86128e6d422a79d21df891f009c
Luca <zbot@pop-os.localdomain>
2021-02-16 19:50:20 -0500

    updated schema adding entries

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,17 @@
 DROP DATABASE IF EXISTS date_DB;
 
 CREATE DATABASE date_DB;
+USE date_DB;
+
+CREATE TABLE `date_DB` (
+
+`id` Int(30) AUTO_INCREMENT NOT NULLL,
+`weather` VARCHAR( 255),
+`restuarant` VARCHAR( 255),
+`food` VARCHAR( 255),
+`movie` VARCHAR( 255),
+`music` VARCHAR( 255),
+
+PRIMARY KEY ( `id` )
+
+);
