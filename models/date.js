840794c4e201f8d90968244a3b9c68cb54b55020
lucpizz <lucpizz@gmail.com>
2021-02-18 19:48:31 -0500

    updated date.js

--- models/date.js
+++ models/date.js
@@ -14,23 +14,3 @@ module.exports = (sequelize, Sequelize) => {
     },
   });
 };
-
-/*
-// Sequelize (capital) references the standard library
-const Sequelize = require("sequelize");
-// sequelize (lowercase) references my connection to the DB.
-const sequelize = require("../config/config.js");
-
-// Creates a "Date" model that matches up with DB
-const Date = sequelize.define("date", {
-  weather: Sequelize.STRING,
-  restuarant: Sequelize.STRING,
-  food: Sequelize.STRING,
-  movie: Sequelize.STRING,
-  music: Sequelize.STRING,
-  movieTime: Sequelize.STRING,
-});
-
-// Makes the Date Model available for other files (will also create a table)
-module.exports = Date;
-*/
