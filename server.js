8f1e3150d699ac6749f1cc2d2300c30093af8e88
john bark <johnbark855@gmail.com>
2021-02-18 20:10:09 -0500

    updated various file including handlebars

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
+
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -19,7 +20,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-const routes = require("./controllers/burgers_controller.js");
+const routes = require("./controllers/dateController.js");
 
 app.use(routes);
 
