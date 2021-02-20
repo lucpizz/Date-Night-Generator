f5d90c81e7ee906f5ab3385d5c3d99d947e6ff76
lucpizz <lucpizz@gmail.com>
2021-02-20 13:47:40 -0500

    updated

--- controllers/dateController.js
+++ controllers/dateController.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const musicapi = require("../routes/music.js")
-console.log(musicapi)
+const musicapi = require("../routes/music.js");
+console.log(musicapi);
 
-router.get("/", async(req, res) => {
- try {
-  const response = await musicapi
-  console.log(response.data)
-  res.render("../views/layouts/main.handlebars" , {body:response.data.data[0].title})
- } catch (error) {
-   console.log(error)
- }
-
+router.get("/", async (req, res) => {
+  try {
+    const response = await musicapi;
+    console.log(response.data);
+    res.render("../views/layouts/main.handlebars", {
+      body: response.data.data[0].title,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
-
-
 
 module.exports = router;
